415c96ba364d6dc5af46b76763f8ba7507d2a974
dataexpert9 <data.expert9@gmail.com>
2018-11-27 00:40:37 +0500

    another

---
+++ SkriblBoxAdminPanel/ViewModels/AddBoxViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasketWebPanel.ViewModels
+{
+    public class AddBoxViewModel : BaseViewModel
+    {
+       
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        
+        public SelectList Categories { get; internal set; }
+
+        public int Category_Id { get; set; }
+
+    }
+
+    public class Video
+    {
+        [Url]
+        public string VideoUrl { get; set; }
+    }
+}
